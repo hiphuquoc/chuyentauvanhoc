03a256ec36857e1520cac06db30d39850aa778de
root <root@DESKTOP-UVJKVE7.localdomain>
2022-08-26 00:30:15 +0700

    add fanpage facebook

--- resources/views/main/blog/sidebar.blade.php
+++ resources/views/main/blog/sidebar.blade.php
@@ -90,12 +90,18 @@
 @endif
 
 <!-- ===== ===== -->
-{{-- <div class="sideBox">
+<div class="sideBox">
     <div class="sideBox_content">
-        <div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/Chuy%E1%BA%BFn-t%C3%A0u-V%C4%83n-H%E1%BB%8Dc-102735669150242" data-tabs="timeline, events, messages" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=412989459053103&amp;container_width=320&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FChuy%25E1%25BA%25BFn-t%25C3%25A0u-V%25C4%2583n-H%25E1%25BB%258Dc-102735669150242&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline%2C%20events%2C%20messages&amp;width=">
+        {{-- <div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/Chuy%E1%BA%BFn-t%C3%A0u-V%C4%83n-H%E1%BB%8Dc-102735669150242" data-tabs="timeline, events, messages" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=412989459053103&amp;container_width=320&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FChuy%25E1%25BA%25BFn-t%25C3%25A0u-V%25C4%2583n-H%25E1%25BB%258Dc-102735669150242&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline%2C%20events%2C%20messages&amp;width=">
             <span style="vertical-align: bottom; width: 320px; height: 500px;">
                 <iframe name="f3a5c93a40e4c44" width="1000px" height="1000px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v14.0/plugins/page.php?adapt_container_width=true&amp;app_id=412989459053103&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df49474c5676e8c%26domain%3Dvanhoc.com%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fvanhoc.com%252Ff2ccfd56e475c14%26relation%3Dparent.parent&amp;container_width=320&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FChuy%25E1%25BA%25BFn-t%25C3%25A0u-V%25C4%2583n-H%25E1%25BB%258Dc-102735669150242&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline%2C%20events%2C%20messages&amp;width=" style="border: none; visibility: visible; width: 320px; height: 500px;" class=""></iframe>
             </span>
+        </div> --}}
+
+        <div class="fb-page" data-href="https://www.facebook.com/chuyentauvanhoc" data-tabs="message" data-width="" data-height="" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="false">
+            <blockquote cite="https://www.facebook.com/chuyentauvanhoc" class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/chuyentauvanhoc">Chuyến tàu Văn Học</a>
+            </blockquote>
         </div>
     </div>
-</div> --}}
+</div>
