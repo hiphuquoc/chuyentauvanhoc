6e406c14d20bc736bc2129a8e5eff0488a96fe18
root <root@DESKTOP-UVJKVE7.localdomain>
2022-08-27 15:53:24 +0700

    fix about home

--- resources/views/main/home/about.blade.php
+++ resources/views/main/home/about.blade.php
@@ -9,13 +9,13 @@
             </h2>
             <div class="homeAbout_content_text">
                 <p>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mình đến với văn học bằng một chữ duyên và quyết định gắn bó với văn chương bằng tình yêu. Tình yêu ấy bây giờ đã kết tinh trong “Chuyến tàu Văn học” – đứa con tinh thần mình ấp ủ bấy lâu nay.
+                    <span class="firstLine"></span>Mình đến với văn học bằng một chữ duyên và quyết định gắn bó với văn chương bằng tình yêu. Tình yêu ấy bây giờ đã kết tinh trong “Chuyến tàu Văn học” – đứa con tinh thần mình ấp ủ bấy lâu nay.
                 </p>
                 <p>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Đối với mình, văn chương không chỉ là những con chữ nằm im trên trang giấy mà văn chương còn là cuộc đời. Bởi vậy, “Chuyến tàu Văn Học” không chỉ là một trang web đơn thuần mà còn là nơi lắng nghe, chia sẻ cảm xúc về cuộc sống và những điều nhỏ bé mà ý nghĩa trong cuộc đời này...!
+                    <span class="firstLine"></span>Đối với mình, văn chương không chỉ là những con chữ nằm im trên trang giấy mà văn chương còn là cuộc đời. Bởi vậy, “Chuyến tàu Văn học” không chỉ là một trang web đơn thuần mà còn là nơi lắng nghe, chia sẻ cảm xúc về cuộc sống và những điều nhỏ bé mà ý nghĩa trong cuộc đời này...!
                 </p>
                 <p>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rất mong “đứa con tinh thần” này sẽ trở thành cầu nối giúp mình lan toả tình yêu văn chương đến tất cả mọi người.
+                    <span class="firstLine"></span>Rất mong “đứa con tinh thần” này sẽ trở thành cầu nối giúp mình lan toả tình yêu văn chương đến tất cả mọi người.
                 </p>
                 <p style="text-align:right;">Hà Nội, ngày 23 tháng 6 năm 2022</p>
                 <p style="text-align:right;">Phạm Thị Ngọc Anh</p>
